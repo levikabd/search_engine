8652b3db5d8a3e067a815669498d79bce078d6ff
Le V <levikabd@yandex.ru>
2025-07-28 00:39:58 +0700

    2025_07_28

---
+++ build/search_engine/CMakeFiles/search_engine.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lev/yadi/DEVEL/41_SearchEngine/search_engine/main.cpp" "search_engine/CMakeFiles/search_engine.dir/main.cpp.o" "gcc" "search_engine/CMakeFiles/search_engine.dir/main.cpp.o.d"
+  "/home/lev/yadi/DEVEL/41_SearchEngine/search_engine/src/converterjson.cpp" "search_engine/CMakeFiles/search_engine.dir/src/converterjson.cpp.o" "gcc" "search_engine/CMakeFiles/search_engine.dir/src/converterjson.cpp.o.d"
+  "/home/lev/yadi/DEVEL/41_SearchEngine/search_engine/src/help.cpp" "search_engine/CMakeFiles/search_engine.dir/src/help.cpp.o" "gcc" "search_engine/CMakeFiles/search_engine.dir/src/help.cpp.o.d"
+  "/home/lev/yadi/DEVEL/41_SearchEngine/search_engine/src/search.cpp" "search_engine/CMakeFiles/search_engine.dir/src/search.cpp.o" "gcc" "search_engine/CMakeFiles/search_engine.dir/src/search.cpp.o.d"
+  "/home/lev/yadi/DEVEL/41_SearchEngine/search_engine/src/search_engine.cpp" "search_engine/CMakeFiles/search_engine.dir/src/search_engine.cpp.o" "gcc" "search_engine/CMakeFiles/search_engine.dir/src/search_engine.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- include/readsettings.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-int checkParametr(int* number, char*** array, bool* help);
-
-//void getSettings(std::string* nameSearch, std::string* rootSearch, bool* fileOnly, std::string* mask, bool* help);
-void getSettings(std::string* nameSearch);
