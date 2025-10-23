9d6d4f418f360915dc88938b096b822ded54be95
levikabd <levikabd@yandex.ru>
2025-10-24 01:10:18 +0700

    correct ignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lev/yadi/DEVEL/41_SearchEngine
-# Build directory: /home/lev/yadi/DEVEL/41_SearchEngine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("search_engine")
-subdirs("test")
--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lev/yadi/DEVEL/41_SearchEngine/test
-# Build directory: /home/lev/yadi/DEVEL/41_SearchEngine/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/lev/yadi/DEVEL/41_SearchEngine/build/test/hello_test[1]_include.cmake")
-subdirs("../_deps/googletest-build")
